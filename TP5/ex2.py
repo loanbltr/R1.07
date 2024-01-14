94353021811845eeedd191e39b98c65d0a1ef2c0
Loan <loan.pub01@gmail.com>
2024-01-14 12:09:00 +0100

    Update ex2.py

--- TP5/ex2.py
+++ TP5/ex2.py
@@ -5,18 +5,17 @@ fullNotes = 0.0
 fullCoef = 0.0
 
 for i in range(5):
-    module = str(input("Veuillez entrer la note du module 1 et le coefficient correspondant :"))
+    module = str(input("Veuillez entrer la note du module 1 et le coefficient correspondant (sous la forme: 'note coef') :"))
     split = module.split(" ")
     note = float(split[0])
     coef = float(split[1])
     if note < 8:
         inf8 = True
-    fullNotes += note
+    fullNotes += note*coef
     fullCoef += coef
 moyenne = fullNotes / fullCoef
-if moyenne > 10:
+if moyenne >= 10:
     mG = True
-
 if mG == True and inf8 == False:
     print("L'élève peut passer")
 else:
